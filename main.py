aa36888d55729d81b4ebd35cc8522c48a690e845
Lucy Ruto <rutolucy968@gmail.com>
2024-05-03 19:00:39 +0300

    App Deployment

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import streamlit as st
-import yfinance as yf
 from datetime import date
 from prophet import Prophet
 from prophet.plot import plot_plotly
 import plotly.graph_objs as go
 import base64
 import pandas as pd
-
+import appdirs as ad
+ad.user_cache_dir = lambda *args: "/tmp"
+import yfinance as yf
 # Define constants
 start_date = "2015-01-01"
 today = date.today().strftime("%Y-%m-%d")
